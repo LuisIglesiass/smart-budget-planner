7329a494e0368c5b04d46f91428d9d47ebbc48ba
Luis Iglesias <luis.iglesias.ramirez@flow4.com>
2026-01-22 20:10:39 +0100

    Integrate Tailwind CSS and @nuxt/image module, add initial app structure (layouts, pages, styles).

---
+++ app/layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="min-h-screen flex flex-col bg-white dark:bg-dark text-black dark:text-white">
+        <header class="flex flex-row items-center py-4">
+            <NuxtImg src="/dev/logo.png" alt="Logo" width="100" />
+            <h1 class="text-2xl font-bold text-primary">Smart Budget Planner</h1>
+        </header>
+
+        <!-- Page Content -->
+        <main class="flex-1">
+            <slot />
+        </main>
+
+        <!-- Footer -->
+        <footer
+            class="bg-gray15 dark:bg-darkComponent border-t border-gray30 dark:border-gray70 text-center py-4 mt-auto"
+        >
+            <p class="text-sm text-gray70 dark:text-gray30">{{ new Date().getFullYear() }} | Luis Iglesias</p>
+        </footer>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({ layout: 'default' });
+</script>
+
+<template>
+    <div class="container-normal">
+        <h1 class="text-4xl font-bold underline text-warning">Hello world! - Home page</h1>
+    </div>
+</template>
+
+<style scoped>
+.container-normal {
+    outline: 2px dotted red;
+}
+</style>
